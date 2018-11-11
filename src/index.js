b091d2a03406a966c4018d5dad9ac5e04b770bb2
Manolis Kalafatis <mankal111@hotmail.com>
2018-11-11 10:55:31 +0200

    created the main component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React, {Component} from 'react'
 
 export default class extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      initialFormula: '',
+      currentFormula: ''
+    }
+  }
+
+  componentDidMount() {
+    this.setState(
+      {
+        initialFormula: this.props.formula,
+        currentFormula: this.props.formula
+      },
+      () => this.props.onFormulaChange(this.state.currentFormula)
+    );
+  }
+
   render() {
     return <div>
-      <h2>Welcome to React components</h2>
+      <h2>{this.state.initialFormula}</h2>
     </div>
   }
 }
