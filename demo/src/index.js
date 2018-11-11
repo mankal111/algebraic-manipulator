c428d606155133b24b4e75f53e08ce96676f2375
Manolis Kalafatis <mankal111@hotmail.com>
2018-11-11 10:55:13 +0200

    created an initial demo for the component

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,13 +1,33 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
 
-import Example from '../../src'
+import AlgebraicManipulator from '../../src'
 
 class Demo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      componentFormula: ''
+    }
+
+    this.formulaChanged = this.formulaChanged.bind(this);
+  }
+
+  formulaChanged(newFormula) {
+    console.log(newFormula);
+    this.setState({componentFormula: newFormula});
+  }
+
   render() {
+    const initialFormula="2y + 4x = 5";
     return <div>
       <h1>algebraic-manipulator Demo</h1>
-      <Example/>
+      <div>The formula given to the component is: {initialFormula}</div>
+      <AlgebraicManipulator
+        formula={initialFormula}
+        onFormulaChange={this.formulaChanged}
+      />
+      <div>The formula returned by the <em>onFormulaChange</em> callback is: {this.state.componentFormula}</div>
     </div>
   }
 }
