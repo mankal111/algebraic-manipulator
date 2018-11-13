f616b65a8eabbdce998de35c19ddf9b04a38c2ae
Manolis Kalafatis <mankal111@hotmail.com>
2018-11-13 15:26:23 +0200

    added input to allow the user to change the initial formula

--- demo/src/index.js
+++ demo/src/index.js
@@ -7,23 +7,29 @@ class Demo extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      componentFormula: ''
+      componentFormula: '',
+      initialFormula: "2y + 4x = 5"
     }
-
+    this.onInputChange = this.onInputChange.bind(this);
     this.formulaChanged = this.formulaChanged.bind(this);
   }
 
   formulaChanged(newFormula) {
-    this.setState({componentFormula: newFormula});
+    this.setState({ componentFormula: newFormula });
+  }
+
+  onInputChange(event) {
+    this.setState({ initialFormula: event.target.value });
   }
 
   render() {
-    const initialFormula="2y + 4x = 5";
     return <div>
       <h1>algebraic-manipulator Demo</h1>
-      <div>The formula given to the component is: {initialFormula}</div>
+      <div>The formula given to the component is: 
+        <input type="text" value={this.state.initialFormula} onChange={this.onInputChange}/>
+      </div>
       <AlgebraicManipulator
-        formula={initialFormula}
+        formula={this.state.initialFormula}
         onFormulaChange={this.formulaChanged}
       />
       <div>The formula returned by the <em>onFormulaChange</em> callback is: {this.state.componentFormula}</div>
