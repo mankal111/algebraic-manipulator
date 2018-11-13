3e2fea0df3556d4120683202bf93b0cee4133050
Manolis Kalafatis <mankal111@hotmail.com>
2018-11-13 17:31:35 +0200

    changed test to a simpler one, just to check that it renders

--- tests/index-test.js
+++ tests/index-test.js
@@ -5,13 +5,9 @@ import {renderToStaticMarkup as render} from 'react-dom/server'
 import Component from 'src/'
 
 describe('Component', () => {
-  it('displays the given formula', () => {
-    const initialFormula="2y + 4x = 5";
+  it('contains h2', () => {
     expect(render(
-      <Component
-        formula={initialFormula}
-        onFormulaChange={() => {}}
-      />
-    )).toContain(initialFormula);
+      <Component />
+    )).toContain("h2");
   })
 })
