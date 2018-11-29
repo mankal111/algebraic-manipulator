ed0bf58204f51ad3eab40c61c0f9ae15d5ea570e
Manolis Kalafatis <mankal111@hotmail.com>
2018-11-29 13:19:18 +0200

    expressionReducer tests

---
+++ tests/reducers/expressionReducer.spec.js
@@ -0,0 +1,30 @@
+import expect from 'expect';
+import reducer from 'src/reducers/expressionReducer';
+import initialState from 'src/reducers/initialState';
+import { SET_EXPRESSION_TREE, SET_SELECTED_EXPRESSION_PATH } from '../../src/actions/actionTypes';
+
+describe('expression reducer', () => {
+    it('returns the initial state', () => {
+        expect(reducer(undefined, {})).toEqual(initialState.expression);
+    });
+
+    it('handles setExpressionTree action', () => {
+        expect(reducer(initialState.expression, {
+            type: SET_EXPRESSION_TREE,
+            tree: 'someTree'
+        })).toEqual({
+            ...initialState.expression,
+            expressionTree: 'someTree'
+        })
+    });
+
+    it('handles setSelectedExpressionPath action', () => {
+        expect(reducer(initialState.expression, {
+            type: SET_SELECTED_EXPRESSION_PATH,
+            path: 'somePath'
+        })).toEqual({
+            ...initialState.expression,
+            selectedExpressionPath: 'somePath'
+        })
+    });
+})
