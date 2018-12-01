ff2ed8714372f1ec0bbdbe277eae8d436ab659ab
Manolis Kalafatis <mankal111@hotmail.com>
2018-12-01 12:28:03 +0200

    eslint and added propTypes and defaultProps

--- src/MainComponent.js
+++ src/MainComponent.js
@@ -1,53 +1,65 @@
-import React, {Component} from 'react';
-import Expression from './Expression';
-import {connect} from 'react-redux';
-import {setExpressionTree} from './actions/expressionActions';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import math from 'mathjs';
+import Expression from './Expression';
+import { setExpressionTree } from './actions/expressionActions';
 
 export class MainComponent extends Component {
   componentDidMount() {
-    this.initialize()
+    this.initialize();
   }
-  
+
   componentDidUpdate(prevProps) {
-    if (this.props.formula !== prevProps.formula) {
+    const { formula } = this.props;
+    if (formula !== prevProps.formula) {
       this.initialize();
     }
   }
-  
+
   initialize() {
-    const expressionTree = math.parse(this.props.formula);
-    this.props.setExpressionTree(expressionTree);
-    this.props.onFormulaChange(expressionTree.toString());
+    const { formula, onFormulaChange } = this.props;
+    const expressionTree = math.parse(formula);
+    setExpressionTree(expressionTree);
+    onFormulaChange(expressionTree.toString());
   }
-  
-  render() { 
+
+  render() {
+    const { expressionTree } = this.props;
     return (
       <div className="formulaContainer">
         {
           <Expression
-            treeRoot={this.props.expressionTree}
-            path='Root'
+            treeRoot={expressionTree}
+            path="Root"
           />
         }
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    expressionTree: state.expression.expressionTree
-  }
-}
+const mapStateToProps = state => ({
+  expressionTree: state.expression.expressionTree,
+});
 
-const mapDispatchToProps = dispatch => {
-  return {
-    setExpressionTree: expressionTree => dispatch(setExpressionTree(expressionTree)) 
-  }
-}
+const mapDispatchToProps = dispatch => ({
+  setExpressionTree: expressionTree => dispatch(setExpressionTree(expressionTree)),
+});
+
+MainComponent.propTypes = {
+  formula: PropTypes.string,
+  onFormulaChange: PropTypes.func,
+  expressionTree: PropTypes.shape,
+};
+
+MainComponent.defaultProps = {
+  formula: '',
+  onFormulaChange: () => {},
+  expressionTree: {},
+};
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
-)(MainComponent)
+  mapDispatchToProps,
+)(MainComponent);
