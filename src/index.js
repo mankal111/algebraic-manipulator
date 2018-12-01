b92ed6ae7587e5a745231dee1f37978f08815b2b
Manolis Kalafatis <mankal111@hotmail.com>
2018-12-01 12:09:26 +0200

    converted index to functional and added propTypes and defaultProps

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
-import React, {Component} from 'react';
-import configureStore from './store/configureStore';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 import MainComponent from './MainComponent';
 import './index.less';
 
 const store = configureStore();
 
-export default class extends Component {
-  render() {
-    return (<Provider store={store}>
-      <MainComponent
-        formula={this.props.formula}
-        onFormulaChange={this.props.onFormulaChange}
-      />
-    </Provider>);
-  }
-}
+const Index = ({ formula, onFormulaChange }) => (
+  <Provider store={store}>
+    <MainComponent
+      formula={formula}
+      onFormulaChange={onFormulaChange}
+    />
+  </Provider>
+);
+
+Index.propTypes = {
+  formula: PropTypes.string,
+  onFormulaChange: PropTypes.func,
+};
+
+Index.defaultProps = {
+  formula: '',
+  onFormulaChange: () => {},
+};
