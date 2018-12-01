ef6a5e12207435d5e7174dac14edf86ea69c4557
Manolis Kalafatis <mankal111@hotmail.com>
2018-12-01 11:50:55 +0200

    eslint

--- src/Expression.js
+++ src/Expression.js
@@ -1,28 +1,31 @@
-import React, {Component} from 'react'
-import OperatorExpression from './OperatorExpression'
+import React from 'react';
+import OperatorExpression from './OperatorExpression';
 
-export default class Expression extends Component {
-  render() {
-    const node = this.props.treeRoot;
-    switch (node.type) {
-      case 'OperatorNode':
-            return <OperatorExpression
-              node={node}
-              path={this.props.path}
-            />;
-      case 'ParenthesisNode':
-        return <span className="parenthesisExpression">
-            (<Expression
-              treeRoot={node.content}
-              path={this.props.path + '.content'}
-            />)
-          </span>
-      case 'ConstantNode':
-        return <span className="constantExpression">{node.value}</span>;
-      case 'SymbolNode':
-        return <span className="symbolExpression">{node.name}</span>;
-      default:
-        return null;
-    }
+export default (props) => {
+  const { treeRoot: node, path } = props;
+  const Expression = this;
+  switch (node.type) {
+  case 'OperatorNode':
+    return (
+      <OperatorExpression
+        node={node}
+        path={path}
+      />
+    );
+  case 'ParenthesisNode':
+    return (
+      <span className="parenthesisExpression">
+        <Expression
+          treeRoot={node.content}
+          path={`${path}.content`}
+        />
+      </span>
+    );
+  case 'ConstantNode':
+    return <span className="constantExpression">{node.value}</span>;
+  case 'SymbolNode':
+    return <span className="symbolExpression">{node.name}</span>;
+  default:
+    return null;
   }
-}
+};
