a138b5c18459e0eefcb83796d615588644c39157
Manolis Kalafatis <mankal111@hotmail.com>
2018-12-02 13:55:33 +0200

    eslint

--- tests/Expression.spec.js
+++ tests/Expression.spec.js
@@ -10,38 +10,38 @@ describe('Expression', () => {
   const expressionComponent = () => {
     if (!wrapper) {
       wrapper = shallow(
-        <Expression {...props} />
+        <Expression {...props} />,
       );
     }
     return wrapper;
-  }
+  };
 
   beforeEach(() => {
     props = {
-      treeRoot: math.parse("2x + (4 - 3y)")
-    }
+      treeRoot: math.parse('2x + (4 - 3y)'),
+    };
     wrapper = undefined;
-  })
+  });
 
-  const setExpressionInProps = exp => props.treeRoot = math.parse(exp);
+  const setExpressionInProps = (exp) => { props.treeRoot = math.parse(exp); };
 
   it('renders an operatorExpression component if the given node is an operator node', () => {
-    setExpressionInProps("1+1");
+    setExpressionInProps('1+1');
     expect(expressionComponent().name()).toMatch('OperatorExpression');
-  })
+  });
 
   it('renders a parenthesisExpression component if the given node is a parenthesis node', () => {
-    setExpressionInProps("(1+1)");
+    setExpressionInProps('(1+1)');
     expect(expressionComponent().hasClass('parenthesisExpression')).toBeTruthy();
-  })
+  });
 
   it('renders a constant component if the given node is a constant node', () => {
-    setExpressionInProps("1");
+    setExpressionInProps('1');
     expect(expressionComponent().hasClass('constantExpression')).toBeTruthy();
-  })
+  });
 
   it('renders a symbol component if the given node is a symbol node', () => {
-    setExpressionInProps("x");
+    setExpressionInProps('x');
     expect(expressionComponent().hasClass('symbolExpression')).toBeTruthy();
-  })
-})
+  });
+});
