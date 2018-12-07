72ebf0f997176b3f515773110b5c362dfb14ee2b
Manolis Kalafatis <mankal111@hotmail.com>
2018-12-07 11:32:10 +0200

    test if expression is valid, outside the component

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,40 +1,68 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import math from 'mathjs';
 
-import AlgebraicManipulator from '../../src'
+import AlgebraicManipulator from '../../src';
 
 class Demo extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      componentFormula: '',
-      initialFormula: "2x + (4 + 3y)"
-    }
+      componentExpression: '',
+      initialExpression: '2x + (4 + 3y)',
+      errorMessage: null,
+    };
     this.onInputChange = this.onInputChange.bind(this);
-    this.formulaChanged = this.formulaChanged.bind(this);
-  }
-
-  formulaChanged(newFormula) {
-    this.setState({ componentFormula: newFormula });
+    this.ExpressionChanged = this.ExpressionChanged.bind(this);
   }
 
   onInputChange(event) {
-    this.setState({ initialFormula: event.target.value });
+    this.setState({ initialExpression: event.target.value });
+    try {
+      math.parse(event.target.value);
+      this.setState({ errorMessage: null });
+    } catch (e) {
+      this.setState({ errorMessage: e.message });
+    }
   }
 
-  render() {
-    return <div>
-      <h1>algebraic-manipulator Demo</h1>
-      <div>The formula given to the component is: 
-        <input type="text" value={this.state.initialFormula} onChange={this.onInputChange}/>
+  ExpressionChanged(newExpression) {
+    this.setState({ componentExpression: newExpression });
+  }
+
+  errorComponent() {
+    const { errorMessage } = this.state;
+    return errorMessage && (
+      <div className="errorContainer">
+        <div>{`There is an error in the given expression: ${errorMessage}.`}</div>
+        <div>Showing the latest correct expression:</div>
       </div>
-      <AlgebraicManipulator
-        formula={this.state.initialFormula}
-        onFormulaChange={this.formulaChanged}
-      />
-      <div>The formula returned by the <em>onFormulaChange</em> callback is: {this.state.componentFormula}</div>
-    </div>
+    );
+  }
+
+
+  render() {
+    const { initialExpression, componentExpression } = this.state;
+    return (
+      <div>
+        <h1>algebraic-manipulator Demo</h1>
+        <div>
+          The Expression given to the component is:
+          <input type="text" value={initialExpression} onChange={this.onInputChange} />
+        </div>
+        <AlgebraicManipulator
+          expression={initialExpression}
+          onExpressionChange={this.ExpressionChanged}
+        />
+        <div>
+          The Expression returned by the
+          <em> onExpressionChange </em>
+          callback is:
+          {componentExpression}
+        </div>
+      </div>
+    );
   }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector('#demo'));
