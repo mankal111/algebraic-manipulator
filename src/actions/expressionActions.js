fe4f4ed69d6a9c6c56b77da0af48e3540ea07478
Manolis Kalafatis <mankal111@hotmail.com>
2018-12-24 21:41:44 +0200

    added args to performAction action

--- src/actions/expressionActions.js
+++ src/actions/expressionActions.js
@@ -8,6 +8,11 @@ export const setSelectedExpression = (path, node) => (
   { type: types.SET_SELECTED_EXPRESSION, path, node }
 );
 
-export const performAction = actionName => (
-  { type: types.PERFORM_ACTION, actionName }
+export const performAction = (actionName, arg1, arg2) => (
+  {
+    type: types.PERFORM_ACTION,
+    actionName,
+    arg1,
+    arg2,
+  }
 );
